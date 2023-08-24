e8d17b2a556cbb294094bc80e2ccf1c383c3ada5
powerfulyang <powerfulyang@qq.com>
2023-08-24 15:31:02 +0800

    fix: 一些问题修复

--- src/options.tsx
+++ src/options.tsx
@@ -10,7 +10,13 @@ browser.tabs.query({ active: true, currentWindow: true }).then(() => {
     createRoot(root).render(
       <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-          <div>Options</div>
+          <div
+            style={{
+              padding: 10,
+            }}
+          >
+            Shortcut：`Command + Option` or `Ctrl + Alt`
+          </div>
         </QueryClientProvider>
       </React.StrictMode>,
     );
